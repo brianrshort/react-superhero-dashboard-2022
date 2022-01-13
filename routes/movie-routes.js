df641e551bddb221988f9952304d98625713a709
Brian Short <brian.ralph.short@gmail.com>
2022-01-13 16:06:16 -0500

    Remove console.logs from movie-routes.js

--- routes/movie-routes.js
+++ routes/movie-routes.js
@@ -8,14 +8,14 @@ let db = require('../models');
 let config = require("../config/config");
 
 router.get("/:topic/:id", (req, res) => {
-    console.log(req.params);
+    //console.log(req.params);
     let int = parseInt(req.params.id);
     db.Movie.findOne({ topic: req.params.topic, movie_order: int  }).then(movie => {
-        console.log(movie);
+        //console.log(movie);
         if (movie) {
             let url = `https://www.omdbapi.com/?t=${movie.title}&y=${movie.release_year}&apikey=${config.omdb}`;
             axios.get(url).then(resp => {
-                console.log(resp.data);
+                //console.log(resp.data);
                 res.send(resp.data);
             });
           } else {
