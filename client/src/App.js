1f37c6863df275bd8438fb97c05a887c1b5a8843
Brian Short <brian.ralph.short@gmail.com>
2022-01-23 16:59:57 -0500

    Update files with new dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Redirect, Route, Routes } from "react-router-dom";
+import axios from "axios";
+
+import Home from "./components/home";
+import Comics from "./components/comics";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/comics" element={<Comics />} />
+      {/* 
+      <Route path="/movies" component={movies} />
+      <Route path="/movies/:topic/:id" component={movie} />
+      <Route path="/comics/:id" component={comic} /> */}
+    </Routes>
+    </Router>
+    
   );
 }
 
