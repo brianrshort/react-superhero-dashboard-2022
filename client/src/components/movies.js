6f4f9d0c891c4d0f8f56f674b82df608b32a5b3d
Brian Short <brian.ralph.short@gmail.com>
2022-01-25 15:14:24 -0500

    Update movies.js to create separate maps for each movie topic

--- client/src/components/movies.js
+++ client/src/components/movies.js
@@ -6,12 +6,18 @@ import axios from "axios";
 import Arr from "./seeds/seeds";
 import config from "./config/config";
 
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
 class Movies extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             movies: [],
-            titles: []
+            topics: [],
+            titles: [],
+            searchField: ""
         }
     }
     
@@ -20,12 +26,23 @@ class Movies extends React.Component {
         let newData = [];
         for (var i = 0; i < Arr[1].length; i++) {
             let movie = Arr[1][i];
-            if (movie) {
+            //console.log(movie.topic);
+            console.log(this.state.topics);
+            //console.log(this.state.topics.indexOf(movie.topic.toString()));
+            this.setState(prev => {
+                console.log(prev.topics);
+                let arr = [...prev.topics, movie.topic];
+                let unique = arr.filter(onlyUnique);
+                return {topics: unique};
+            })
+            //console.log(this.state.topics);
+            //if (movie) {
                 let url = `https://www.omdbapi.com/?t=${movie.title}&y=${movie.release_year}&apikey=${config.omdb}`;
                 axios.get(url).then(resp => {
                     //console.log(resp.data.Title);
                     const obj = {};
                     obj.title = resp.data.Title;
+                    obj.topic = movie.topic;
                     obj.order = movie.movie_order;
                     //console.log(obj)
                     newData.push(obj);
@@ -33,12 +50,15 @@ class Movies extends React.Component {
                     let sortData = newData.sort((a, b) => {
                         return a.order - b.order;
                     });
+                    let alphaSort = sortData.sort((a, b) => {
+                        return a.topic - b.topic;
+                    })
                     //console.log(sortData);
-                    this.setState({titles: sortData});
+                    this.setState({titles: alphaSort});
                     //this.setState({data: newData});
                     //console.log("State: " + JSON.stringify(this.state.data));
                 })
-              } 
+              //} 
         }
 
     }
@@ -58,13 +78,38 @@ class Movies extends React.Component {
 
     render() {
         
-    let moviesMap = this.state.titles.map((obj) => {
-        return <p key={obj.order}>{obj.title}</p>;
+
+        let filmMap = (string) => {
+            const objMap = this.state.titles.map(obj => {
+                if (obj.topic === string) {
+                    return <p key={obj.topic + obj.order}>{obj.title}</p>;
+                }
+            })
+            return objMap;
+        }
+
+    let moviesMap = this.state.topics.map((string) => {
+        return (
+            <div key={string}>
+                <h1>{string}</h1>
+                {filmMap(string)}
+            </div>
+        )
+        
+        // this.state.titles.map(obj => {
+        //     if (obj.topic === string) {
+        //         return <p key={obj.topic + obj.order}>{obj.title}</p>;
+        //     }
+        // }) 
     })
+
+    
     
     return (
     <div className="container">
         Movies
+        <hr />
+        <input type="search" placeholder="search comics" onChange={e => this.setState({searchField: e.target.value})} />
         {moviesMap}
     </div>
     )
