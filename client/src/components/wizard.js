64c311a9940114f0d717d01b939e9d42ea9e796c
Brian Short <brian.ralph.short@gmail.com>
2022-02-10 20:22:18 -0500

    Add ranks array and wizard.js wizard character creation file

---
+++ client/src/components/wizard.js
@@ -0,0 +1,35 @@
+const ranks = [
+    {
+        range: [1,5],
+        property: "Feeble"
+    },
+    {
+        range: [6,10],
+        property: "Poor"
+    },
+    {
+        range: [11, 20],
+        property: "Typical"
+    },
+    {
+        range: [21, 40],
+        property: "Good"
+    },
+    {
+        range: [41, 60],
+        property: "Excellent"
+    },
+    {
+        range: [61,80],
+        property: "Remarkable"
+    },
+    {
+        range: [81,96],
+        property: "Incredible"
+    },
+    {
+        range: [97, 100],
+        property: "Amazing"
+    }
+];
+
