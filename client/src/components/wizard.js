b57667eabe73267de994babf5449c43088f7a5d7
Brian Short <brian.ralph.short@gmail.com>
2022-02-10 20:24:16 -0500

    Add power range array to wizard.js

--- client/src/components/wizard.js
+++ client/src/components/wizard.js
@@ -43,3 +43,56 @@ const abilities = [
     "Psyche"
 ]
 
+
+
+const powerRange = [
+    {
+        range: [1,12],
+        property: 1
+    },
+    {
+        range: [13,26],
+        property: 2
+    },
+    {
+        range: [27,41],
+        property: 3
+    },
+    {
+        range: [42,55],
+        property: 4
+    },
+    {
+        range: [56,66],
+        property: 5
+    },
+    {
+        range: [67,75],
+        property: 6
+    },
+    {
+        range: [76, 83],
+        property: 7
+    },
+    {
+        range: [84,89],
+        property: 8
+    },
+    {
+        range: [90,94],
+        property: 9
+    },
+    {
+        range: [95, 97],
+        property: 10
+    },
+    {
+        range: [98, 99],
+        property: 12
+    },
+    {
+        range: [100,100],
+        property: 13
+    }
+]
+
