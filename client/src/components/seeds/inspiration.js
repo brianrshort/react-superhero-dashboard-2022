f7c90f012b34ff9383fbf82c49a5b3734277bc8e
Brian Short <brian.ralph.short@gmail.com>
2022-02-12 10:34:33 -0500

    Created an inspiration.js file to collect data on random elements to eventually be displayed on the homepage

---
+++ client/src/components/seeds/inspiration.js
@@ -0,0 +1,17 @@
+const inspiration = [
+    {
+        type: "video",
+        url: "https://youtu.be/xJl4Al2hk6I",
+        name: "In the forest"
+    },
+    {
+        type: "video",
+        url: "https://youtu.be/sefu7bJgMqA",
+        name: "Surya Namaskar Mantra"
+    },
+    {
+        type: "video",
+        url: "https://youtu.be/i7nWIy4E4-E",
+        name: "Morning mantra"
+    },
+];
