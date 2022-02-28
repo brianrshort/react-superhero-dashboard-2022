3b0a7088dc1f17f7f94fa7de0cc8970e36d1dd3e
Brian Short <brian.ralph.short@gmail.com>
2022-02-28 10:54:57 -0500

    Add emptyComicvine.js and emptyMarvel.js; state needs to begin with an empty object before it will populate correctly

---
+++ client/src/components/seeds/emptyComicvine.js
@@ -0,0 +1,18 @@
+const emptyComicvine = {
+    image: {
+        small_url: "https://thumbs.dreamstime.com/b/transparent-designer-must-have-fake-background-39672616.jpg"
+    },
+    volume: {
+        name: ""
+    },
+    issue_number: "",
+    cover_date: new Date(),
+    person_credits: [],
+    character_credits: [],
+    location_credits: [],
+    object_credits: [],
+    concept_credits: [],
+    site_detail_url: ""
+}
+
+export default emptyComicvine;
---
+++ client/src/components/seeds/emptyMarvel.js
@@ -0,0 +1,25 @@
+const emptyMarvel = {
+    images: [{
+        path: "https://thumbs.dreamstime.com/b/transparent-designer-must-have-fake-background-39672616",
+        extension: "jpg"
+    }],
+    dates: [{
+        date: new Date()
+    }],
+    title: "",
+    series: {
+        name: ""
+    },
+    characters: {
+        items: []
+    },
+    creators: {
+        items: []
+    },
+    description: "",
+    urls: [{
+        url: ""
+    }]
+}
+
+export default emptyMarvel;
