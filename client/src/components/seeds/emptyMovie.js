8415cf971b3bb3e7e54c52de552d5b11093d2053
Brian Short <brian.ralph.short@gmail.com>
2022-03-01 09:11:12 -0500

    Add emptyOmbd.js to seed the movie component with data before API calls are made

---
+++ client/src/components/seeds/emptyMovie.js
@@ -0,0 +1,9 @@
+const emptyMovie = {
+    movie: "",
+    movie_order: 0,
+    release_year: 0, 
+    title: "",
+    topic: ""
+}
+
+export default emptyMovie;
---
+++ client/src/components/seeds/emptyOmdb.js
@@ -0,0 +1,16 @@
+const emptyOMBD = {
+    Poster: "https://thumbs.dreamstime.com/b/transparent-designer-must-have-fake-background-39672616.jpg",
+    Title: "",
+    Runtime: "",
+    Year: "",
+    Genre: "",
+    Plot: "",
+    Director: "",
+    Writer: "",
+    Actors: "",
+    BoxOffice: "",
+    Awards: "",
+    Ratings: []
+}
+
+export default emptyOMBD;
