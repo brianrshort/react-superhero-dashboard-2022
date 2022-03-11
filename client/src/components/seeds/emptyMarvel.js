146941830d32de9e21aaee7282d2a4f5462e0884
Brian Short <brian.ralph.short@gmail.com>
2022-03-11 08:56:07 -0500

    Update default jpeg location url

--- client/src/components/seeds/emptyMarvel.js
+++ client/src/components/seeds/emptyMarvel.js
@@ -1,6 +1,8 @@
 const emptyMarvel = {
     images: [{
-        path: "https://thumbs.dreamstime.com/b/transparent-designer-must-have-fake-background-39672616",
+        //path: "https://thumbs.dreamstime.com/b/transparent-designer-must-have-fake-background-39672616",
+        //extension: "jpg"
+        path: "https://img.freepik.com/free-vector/mystical-golden-frame-black-background_53876-115276",
         extension: "jpg"
     }],
     dates: [{
